e215c0829282bb89a257172034e0a664b023526e
fiorina29 <fiorinaroycee@outlook.fr>
2024-03-06 15:29:01 +0100

    test

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// App.js
+
+import React from 'react';
+import Test from "./component/accueil/test";
+
+function App() {
+    return (
+        <div>
+            <Test />
+        </div>
+    );
+}
+
+export default App;
+
---
+++ src/component/accueil/test.jsx
@@ -0,0 +1,14 @@
+// ./component/accueil/test.js
+
+import React from 'react';
+
+const Test = () => {
+    return (
+        <div>
+            <h2>Agenda mis à jour en temps réel</h2>
+        </div>
+    );
+};
+
+export default Test;
+
