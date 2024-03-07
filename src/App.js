49c5b05584d3b85210d068fd9b6f680a56c3c143
BaseFeature4 <demoraisth@cy-tech.fr>
2024-03-07 11:23:02 +0100

    Base feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Contact from "./Components/Contact/Contact"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <Contact/>
       </header>
     </div>
   );
