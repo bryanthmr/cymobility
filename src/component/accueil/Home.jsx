52b5498b6987f8db9eb8959f86df1febf9bbf2e3
fiorina29 <fiorinaroycee@outlook.fr>
2024-03-10 18:13:39 +0100

    version 1 de l'acceuil avec css
    cette fois-ci je sais ce que je fais :)

--- src/component/accueil/Home.jsx
+++ src/component/accueil/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Slide} from "react-slideshow-image";
 import '../../styles/_home.scss';
 import 'react-slideshow-image/dist/styles.css';
+import {Card} from 'react-bootstrap';
 
 
 const Home = () => {
@@ -14,6 +15,7 @@ const Home = () => {
         '/homeImage6.jpg',
         '/homeImage7.jpg'
     ];
+
 
     return (
         <div className='home'>
@@ -32,9 +34,46 @@ const Home = () => {
                 </Slide>
             </div>
 
-            <div className='nosServices'>
-                <h1>Planifier votre mobilité international avec CyMobility</h1>
+
+            <div className="nosServices">
+                <h1>Nos services</h1>
+
+                <div className="cartes">
+                    <Card className="card1">
+                        <Card.Img variant="top" src='/homeImage7.jpg'/>
+                        <Card.Body>
+                            <Card.Title>Choisis ta destination </Card.Title>
+
+                        </Card.Body>
+                    </Card>
+
+                    <Card className="card2">
+                        <Card.Img variant="top" src='/homeImage5.jpg'/>
+                        <Card.Body>
+                            <Card.Title>Sélectionnes tes spécialités</Card.Title>
+
+                        </Card.Body>
+                    </Card>
+
+                    <Card className="card3">
+                        <Card.Img variant="top" src='/homeImage6.jpg'/>
+                        <Card.Body>
+                            <Card.Title>Découvres toutes les opportunités</Card.Title>
+
+                        </Card.Body>
+                    </Card>
+
+                </div>
             </div>
+
+            <div className="contact">
+                <h1>Contact</h1>
+                <h3>Cergy, France</h3>
+                <h3>info@mysite.com</h3>
+                <h3>123-456-7890</h3>
+            </div>
+
+
 
 
         </div>
