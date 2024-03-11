628865a338372beb295afe90683febb437ac2daa
BaseFeature4 <demoraisth@cy-tech.fr>
2024-03-11 15:08:04 +0100

    Finissions de la base de la page de contact et de son CSS

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -8,21 +8,24 @@ export default function Contact(){
     return(
         <>
             <div id={'contactContent'}>
-                <div id={'adressesContent'}>
-                    <h2>Contactez-nous</h2>
-                    <p id={'adresses'}>
-                        47 rue des Couronnes 75020 Paris, <br/>
-                        France <br/><br/>
-                        01 23 45 67 89 <br/>
-                        bryan@cymobility.go.yo.fr<br/>
-                    </p>
+                <div id={'adressesContent1'}>
+                    <div id={'adressesContent2'}>
+                        <h2> Contactez-nous</h2>
+                        <p id={'adresses'}>
+                            47 rue des Couronnes 75020 Paris, <br/>
+                            France <br/><br/>
+                            01 23 45 67 89 <br/>
+                            bryan@cymobility.go.yo.fr<br/>
+                        </p>
+                    </div>
                 </div>
+
                 <div id={'formContent'}>
                     <form id={'contactForm'} method={'POST'} action={'#'}>
                         <div className={'form-row'}>
                             <div>
                                 <label htmlFor={'nom'}>Nom</label>
-                                <input style={{height: '30px' }} id={'nom'} type={'text'}/>
+                                <input style={{height: '30px'}} id={'nom'} type={'text'}/>
                             </div>
                             <div>
                                 <label htmlFor={'prenom'}>Prénom</label>
