496ea8319e1e14b877169eecdc86aed50fa71aa6
bryanthmr <thumereaub@cy-tech.fr>
2024-05-15 17:45:29 +0200

    CA MARCHE

--- src/frontend/Components/Destination/Destination.jsx
+++ src/frontend/Components/Destination/Destination.jsx
@@ -1,40 +1,14 @@
-import { useEffect, useState } from 'react';
-import { Card } from 'react-bootstrap';
-import homeImage6 from '../../img/homeImage6.jpg';
-import './MenuSpe.css';
+import Offre from "./Offre";
+import MenuDest from "./MenuDest/MenuDest";
 import MenuSpe from "./MenuSpe/MenuSpe";
 
-export default function Destination({ isVisible }) {
-    const [specialites, setSpecialites] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('https://cymobility.go.yo.fr/apiLyl/data')
-                const result = await response.json();
-                console.log(result);
-
-                // Extraire les spécialités et supprimer les doublons
-                setSpecialites(result);
-
-
-
-            } catch (error) {
-                console.log('error', error);
-            }
-        };
-
-        fetchData(); // Ajouter cette ligne pour gérer les erreurs
-    }, []);
-
-    return isVisible ? (
-            <>
-
-                <MenuSpe/>
-               </>
-
-
-
-        ) :
-        null;
+export default function Destination({isVisible}){
+    return isVisible?(
+        <>
+            <MenuSpe/>
+            <MenuDest/>
+            <Offre/>
+        </>
+    ):null;
 }
