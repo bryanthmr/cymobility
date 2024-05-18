e359ad111a9b23c7347886329ad55bb6be101ef2
BaseFeature4 <demoraisth@cy-tech.fr>
2024-05-18 11:37:13 +0200

    Variable de session

--- src/backend/App.js
+++ src/backend/App.js
@@ -1,42 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const argon2 = require('argon2');
 const maria = require('mariadb');
 
 const app = express();
 
 const pool = maria.createPool({
-    host: 'localhost',
-    user: 'myjuffzf_userTest',
-    password: 'L#NO5!NTlOO2@uT,Z5',
-    port: '3306',
-    database: 'myjuffzf_Cymobility',
+    host: 'localhost', // Assurez-vous que l'hôte est correct
+    user: 'myjuffzf_userTest', // Assurez-vous que l'utilisateur est correct
+    password: 'L#NO5!NTlOO2@uT,Z5', // Assurez-vous que le mot de passe est correct
+    port: '3306', // Assurez-vous que le port est correct
+    database: 'myjuffzf_Cymobility', // Assurez-vous que la base de données est correcte
     connectionLimit: 5,
 });
 
-// Configuration CORS
-const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
-    credentials: true,
-};
-
-app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-app.options('*', cors(corsOptions)); // Gérer explicitement les requêtes pré-vol
-
-// Middleware pour inclure les en-têtes CORS dans toutes les réponses
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-// Routes
 app.post('/apiEya/connexion', async (req, res, next) => {
     let conn;
     const { login, password } = req.body;
@@ -44,28 +32,19 @@ app.post('/apiEya/connexion', async (req, res, next) => {
     try {
         conn = await pool.getConnection();
         console.log('Connected to the database');
-        const sql = 'SELECT id_eleve, prenom, mdp FROM Eleve WHERE id_eleve = ?';
-        const result = await conn.query(sql, [login]);
+        const sql = 'SELECT id_eleve, prenom FROM Eleve WHERE id_eleve = ? AND mdp = ?';
+        const result = await conn.query(sql, [login, password]);
         console.log('Query executed, result:', result);
         if (result.length > 0) {
             const user = result[0];
             console.log('User found:', user);
 
-            const passwordMatch = await argon2.verify(user.mdp, password);
-            console.log('Password match:', passwordMatch);
+            // Convert BigInt to string
+            const userId = user.id_eleve.toString();
+            const userPrenom= user.prenom;
 
-            if (passwordMatch) {
-                console.log('User authenticated successfully');
-                let userId = user.id_eleve;
-                const userPrenom = user.prenom;
-
-                res.status(200).json({ success: true, message: 'Connexion réussie', user: { id: userId, name: userPrenom } });
-            } else {
-                console.log('Password does not match');
-                res.status(401).json({ success: false, message: 'Identifiants incorrects' });
-            }
+            res.status(200).json({ success: true, message: 'Connexion réussie', user: { id: userId, name: userPrenom } });
         } else {
-            console.log('User not found');
             res.status(401).json({ success: false, message: 'Identifiants incorrects' });
         }
     } catch (error) {
@@ -76,6 +55,7 @@ app.post('/apiEya/connexion', async (req, res, next) => {
     }
 });
 
+
 app.post('/apiEya/inscription', async (req, res, next) => {
     let conn;
     const { nom1, nom2, mail, idEleve, password, date, niveauEtude } = req.body;
@@ -84,13 +64,8 @@ app.post('/apiEya/inscription', async (req, res, next) => {
 
     try {
         conn = await pool.getConnection();
-        const hashedPassword = await argon2.hash(password);
-        console.log('Hashed password:', hashedPassword);
-
-        const sql = 'INSERT INTO Eleve (nom, prenom, mail, id_eleve, mdp, date, id_etude) VALUES (?, ?, ?, ?, ?, ?, (SELECT id_etude FROM Etude WHERE niveau = ?))';
-        const result = await conn.query(sql, [nom1, nom2, mail, idEleve, hashedPassword, date, niveauEtude]);
-        console.log('User registered:', result);
-
+        const sql = 'INSERT INTO Eleve (nom, prenom, mail, id_eleve, mdp, date_naissance, id_etude) VALUES (?, ?, ?, ?, ?, ?, (SELECT id_etude FROM Etude WHERE niveau = ?))';
+        const result = await conn.query(sql, [nom1, nom2, mail, idEleve, password, date, niveauEtude]);
         res.status(200).send({ success: true, message: 'Inscription réussie' });
     } catch (error) {
         console.error('Database error:', error);
@@ -107,37 +82,6 @@ app.get('/apiEya/checkSession', (req, res) => {
     } else {
         res.status(401).send({ loggedIn: false });
     }
-});
-
-app.use('/api/test', (req, res, next) => {
-    const stuff = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(stuff);
-});
-
-// Middleware d'erreur
-app.use((err, req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    res.status(err.status || 500);
-    res.json({ success: false, message: err.message });
 });
 
 module.exports = app;
--- src/frontend/Components/Login/LoginForm.jsx
+++ src/frontend/Components/Login/LoginForm.jsx
@@ -11,6 +11,7 @@ export default function Login({ isVisible, showSignin, showHome }) {
 
     const fetchData = async () => {
         const credentials = { login, password };
+        console.log('Fetching data with credentials:', credentials);
 
         try {
             const response = await fetch('https://cymobility.go.yo.fr/apiEya/connexion', {
@@ -21,22 +22,30 @@ export default function Login({ isVisible, showSignin, showHome }) {
                 body: JSON.stringify(credentials),
             });
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
+            console.log('Response status:', response.status);
 
-            const data = await response.json();
+            // Affichez le contenu brut de la réponse
+            const text = await response.text();
+            console.log('Raw response text:', text);
+
+            // Essayez de convertir le texte en JSON
+            const data = JSON.parse(text);
+            console.log('Response data:', data);
 
             if (data.success) {
+                console.log('Connexion réussie');
                 const user = data.user;
+                console.log('User data:', user);
                 if (user && user.id && user.name) {
                     localStorage.setItem('user', JSON.stringify(user)); // Stocker les informations de l'utilisateur dans localStorage
                     setAuthState({ loggedIn: true, user }); // Mettre à jour le contexte d'authentification
                     showHome('accueil', true); // Rediriger vers l'accueil après une connexion réussie
                 } else {
+                    console.error('Invalid user data:', user);
                     setError(true);
                 }
             } else {
+                console.log('Échec de la connexion');
                 setError(true);
             }
         } catch (error) {
@@ -44,9 +53,9 @@ export default function Login({ isVisible, showSignin, showHome }) {
             setError(true);
         }
     };
-
     const handleSubmit = (e) => {
         e.preventDefault();
+        console.log('Form submitted with login:', login, 'password:', password);
         fetchData();
     };
 
